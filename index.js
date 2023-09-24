64c5942810275cc40dfd8a1b50106e1aa9fd0db3
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2023-09-24 23:54:36 +0300

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-var sayHi = function () { return console.log('Hi'); };
-sayHi();
-var sumArgs = function (a, b) { return a + b; };
-var user = {
-    name: 'John',
-    age: 35,
-    isAdmin: false
-}, nop;
--- index.ts
+++ index.ts
@@ -16,3 +16,47 @@ const user: User = {
     age: 35,
     isAdmin: false
 }
+
+//Arrays 
+const stringArray: string[] = ['a', 'b', 'c'];
+// stringArray.push(1) //error
+
+const anyTypesArray: any[] = ['a', 1, true, {a: 1}];
+
+//Tuples 
+const numericTuple = [1, 2] as const;
+//numericTuple.push(3); //error
+
+//Functions 
+function printMsg(msg: string): void {
+    console.log(msg);
+}
+
+//Arrow 
+const printMsg2 = (msg: string): void => {
+    console.log(msg);
+}
+
+//Functions optional parameters
+function printMsg3(msg: string, additionalMsg?: string): void {
+    console.log(`${msg}${additionalMsg}`);
+}
+
+//Functions default parameters
+function printMsg4(msg: string, additionalMsg: string = 'default string'): void {
+    console.log(`${msg}${additionalMsg}`);
+}
+
+printMsg4('1st');
+printMsg4('1st', '2nd');
+
+//Functions interface
+interface ISumFunc {
+    (a: number, b: number): number;
+}
+
+let sum: ISumFunc;
+
+sum = (a: number, b: number) => a + b; //okay 
+
+//sum = (a: string, b: number) => a + b; //error
